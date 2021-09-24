9cf8971d20a15e91063e7100a3533e3fb1ba69a8
onava <enava23@yahoo.com>
2021-09-23 20:17:21 -0400

    fetch service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -45,3 +45,32 @@ self.addEventListener('activate', function (event) {
 })
 
 // Fetch
+self.addEventListener('fetch', function (event) {
+  if (event.request.url.includes('/api/')) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+        .then(response => {
+          if (response.satus === 200) {
+            cache.put(event.request.url, response.clone());
+          }
+          return response;
+        })
+        .catch(err => {
+          return cache.match(event.request);
+        });
+      })
+      .catch(err => console.log(err))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    })
+  )
+})
+
