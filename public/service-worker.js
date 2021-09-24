a51d961b443b427e29c7f9ece8b10fa982f1c628
onava <enava23@yahoo.com>
2021-09-23 20:10:19 -0400

    fetch service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -28,3 +28,20 @@ self.addEventListener('install', function (event) {
 })
 
 // Activate
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log('Removing old cache data', key);
+          }
+        })
+      )
+    })
+  )
+
+  self.clients.claim();
+})
+
+// Fetch
